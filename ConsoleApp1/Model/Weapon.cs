99d50cc06b803ff246a417d45c148b8383f7d941
dunyamaliahmedli <tu7bg7w6d@code.edu.az>
2022-03-15 17:27:34 +0400

    added test file

---
+++ ConsoleApp1/Model/Weapon.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Model
+{
+    class Weapon
+    {
+        public int gulletutumu;
+        public int gullesayi;
+        public double bosalmavaxti;
+        public bool autoOrSimple;
+
+
+        public Weapon(int gulletutumu, int gullesayi, double bosalmavaxti, bool atismodu)
+        {
+            this.gulletutumu = gulletutumu; this.gullesayi = gullesayi; this.bosalmavaxti = bosalmavaxti; this.autoOrSimple = autoOrSimple;
+
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("Gulle tutumu:"+ gulletutumu+"  gUlle sayi:"+ gullesayi+"  Bosalma vaxti:"+bosalmavaxti+"   Atis modu:"+ autoOrSimple);
+        }
+
+        public void Shoot()
+        {
+            if (gullesayi > 0)
+            {
+
+                int atis = 0;
+                atis = gullesayi - 1;
+                Console.WriteLine("Ates acildi....Partt " + "qalan gulle sayi: " + atis);
+            }
+
+
+            else
+            {
+                Console.WriteLine("Gulle yoxdur");
+
+            }
+        }
+        public void Fire()
+        {
+            if (gullesayi > 0)
+            {
+                double vaxt = 3.5;
+                int atis = 0;
+                atis = gullesayi - gullesayi;
+                vaxt = gullesayi * bosalmavaxti / gulletutumu;
+                Console.WriteLine("Ates acildi....Partt,Part.. " + "qalan gulle sayi: " + atis + "Nece saniye bitdi :" + vaxt);
+            }
+            else
+            {
+                Console.WriteLine("Gulle yoxdur");
+
+            }
+
+        }
+        public void GetRemainBulletCount()
+        {
+
+            int lazim = 0;
+            lazim = gulletutumu - gullesayi;
+            Console.WriteLine("Lazim olan gullesayi: " + lazim);
+        }
+
+        public void Reload()
+        {
+
+            int lazim = gulletutumu - gullesayi;
+            int cem = gullesayi + lazim;
+            Console.WriteLine("Gulle doldurmagi ucun lazim olan say " + lazim + "  Doldurulduqdan sonra gulle sayi: " + cem);
+        }
+        public void ChangeFireMode()
+        {
+            //Console.WriteLine("true auto,false");
+            bool secim = bool.Parse(Console.ReadLine());
+
+            if (secim == true)
+            {
+                autoOrSimple = true;
+            }
+            else if (secim == false)
+            {
+                autoOrSimple = false;
+            }
+            else
+            {
+                Console.WriteLine("False yada Trueden istifade edin");
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace ConsoleApp1.Model
+
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Xos gelmisiz online atis Merkezine");
+            Console.WriteLine("Gulle tutumunu qeyd edin :");
+            int gulletutumu = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gulle sayini qeyd edin:");
+            int gullesayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Gulle bosalma vaxtini daxil edin :");
+            double bosalmavaxti = double.Parse(Console.ReadLine());
+            Console.WriteLine("atis modunu secin:");
+            bool atismodu = bool.Parse(Console.ReadLine());
+
+            Weapon Silah = new Weapon(gulletutumu, gullesayi, bosalmavaxti, atismodu);
+
+
+            int key = 0;
+
+            do
+
+            {
+                Console.WriteLine("Zehmet olmasa secim edin(1-6):");
+                key = Convert.ToInt32(Console.ReadLine());
+
+                switch (key)
+                {
+                    case 1:
+
+                        Silah.Shoot();
+                        break;
+                    case 2:
+
+                        Silah.Fire();
+                        break;
+                    case 3:
+
+                        Silah.GetRemainBulletCount();
+                        break;
+                    case 4:
+
+                        Silah.Reload();
+                        break;
+                    case 5:
+
+
+                        Silah.ChangeFireMode();
+                        break;
+
+                    case 6:
+                        Console.WriteLine("Sagolunnn");
+                        break;
+                    case 0:
+                        Silah.GetInfo();
+                        break;
+
+                    default:
+                        Console.WriteLine("Zehmet olmasa duzgun reqem secin");
+                        break;
+
+                }
+            }
+            while (key != 6);
+            {   }
+
+
+
+
+
+            }
+            }
+
+
+    }
+
+
